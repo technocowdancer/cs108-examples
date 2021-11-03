e1ce44574680167ddc91e4af18986d7d3ad75800
Kelsey Tebo <ktebo@bu.edu>
2021-11-03 15:47:07 -0400

    Created Profile module

---
+++ mini_fb/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Profile(models.Model):
+    '''
+    Will model the data of the attributes of FB users. 
+    Model will need to include the following data attributes: 
+    first name, last name, city, email address, and a profile image url
+    '''
+    first_name = models.TextField(blank=True)
+    last_name = models.TextField(blank=True)
+    city = models.TextField(blank=True)
+    email = models.TextField(blank=True)
+    profile_image_url = models.TextField(blank=True)
+
+
+    def __str__(self):
+        '''return a string representation of this object.'''
+        return f'{self.first_name} + {self.last_name}'
