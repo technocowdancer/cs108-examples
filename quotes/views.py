7bddc9bda62d0c7bd42aea6e4d2f6e7858206384
Kelsey Tebo <ktebo@bu.edu>
2021-11-04 08:58:59 -0400

    added a new view

--- quotes/views.py
+++ quotes/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Quote
+import random
 
 class HomePageView(ListView):
     '''SHow a listing of Quotes.'''
     model = Quote # retrieve Quote objects from database
     template_name = "quotes/home.html"
     context_object_name = "quotes"
-# Create your views here.
+
+
+class QuotePageView(DetailView):
+    '''Display  single quote object.'''
+    model = Quote                        # Retrieve Quote objects from the database
+    template_name = "quotes/quote.html"  # Delegate the display to this teplate
+    context_object_name = "quote"        # Use this variable name in the template 
+
+class RandomQuotePageView(DetailView):
+    '''Display  single quote object.'''
+    model = Quote                        # Retrieve Quote objects from the database
+    template_name = "quotes/quote.html"  # Delegate the display to this teplate
+    context_object_name = "quote"        # Use this variable name in the template 
+
+    def get_object(self):
+        '''Select one quote at random'''
+
+        # obtain all quotes using the object manager
+        quotes = Quote.objects.all()
+
+        # select one at random
+        q = random.choice(quotes)
+        return q
+
