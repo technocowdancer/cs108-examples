6ded88d38065bf34345d32a1347c477dc48732a0
Kelsey Tebo <ktebo@bu.edu>
2021-11-09 11:23:02 -0500

    added post status message feature

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -8,5 +8,5 @@ urlpatterns =[
     path('profile/<int:pk>', ShowProfilePageView.as_view(), name="show_profile_page"),
     path('create_profile', CreateProfileView.as_view(), name='create_profile'),
     path('profile/<int:pk>/update', UpdateProfileView.as_view(), name='update_profile'),
-    #path('/mini_fb/profile/<int:pk>/post_status', post_status_message.as_view(), name='post_status_message'),
+    path('profile/<int:pk>/post_status', post_status_message, name='post_status_message'),
 ]
